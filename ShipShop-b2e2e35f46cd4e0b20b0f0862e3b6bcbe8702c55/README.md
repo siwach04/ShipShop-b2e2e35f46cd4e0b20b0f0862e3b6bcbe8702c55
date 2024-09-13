af0d79bf4e7b4bda5b3c553c721f823f999659a0
siwach04 <135362895+siwach04@users.noreply.github.com>
2024-09-13 19:38:50 +0530

    Delete ShipShop-b2e2e35f46cd4e0b20b0f0862e3b6bcbe8702c55 directory

--- ShipShop-b2e2e35f46cd4e0b20b0f0862e3b6bcbe8702c55/README.md
+++
@@ -1,2 +0,0 @@
-"# ShipShop" 
-"# ShipShop" 
--- ShipShop-b2e2e35f46cd4e0b20b0f0862e3b6bcbe8702c55/ShipShop/Loginpage/loginpage.js
+++
@@ -1,9 +0,0 @@
-let menutoggle = document.querySelector('#menuslide');
-let buttontoggle = document.querySelector('#menubutton');
-let menulist = document.querySelector('#listslide');
-buttontoggle.addEventListener('click',function(){
-    menutoggle.classList.toggle('active');
-});
-buttontoggle.addEventListener('reload',function(){
-    menutoggle.classList.toggle('not-active');
-});
